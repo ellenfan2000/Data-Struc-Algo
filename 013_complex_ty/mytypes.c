c06f0e390af0ca93bec9c3042e5d7044becd3f3c
rf96 <rf96@duke.edu>
2022-09-02 12:55:57 -0400

    013_complex_ty answers

---
+++ 013_complex_ty/mytypes.c
@@ -0,0 +1,8 @@
+struct _complex_number_t {
+  double real_part;
+  double imaginary_part;
+};
+typedef struct _complex_number_t complex_number_t;
+
+enum _animal_t { CAT, FISH, SNAKE, DOG, HORSE };
+typedef enum _animal_t animal_t;
