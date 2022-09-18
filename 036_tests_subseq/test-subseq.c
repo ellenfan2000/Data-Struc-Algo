2ee434f31561144a0dd896fabadcb7838639d301
rf96 <rf96@duke.edu>
2022-09-18 15:09:51 -0400

    036

--- 036_tests_subseq/test-subseq.c
+++ 036_tests_subseq/test-subseq.c
@@ -5,12 +5,12 @@ size_t maxSeq(int * array, size_t n);
 
 int main(void) {
   size_t n1 = 8;
-  int array1[8] = {
+  int array1[] = {
       1, 2, 3, 1, 2, 3, 4, 5};  //may only consider the first consequtive sequence
-  size_t n2 = 10;
-  int array2[10] = {1, 2, 1, 3, 5, 7, 2, 4, 6, 9};  //consecutive
+  size_t n2 = 11;
+  int array2[] = {1, 2, 1, 3, 5, 7, 2, 4, 6, 9, 10};  //consecutive
   size_t n3 = 6;
-  int array3[6] = {1, 2, 2, 3, 4, 5};  //wrongly use >= and >
+  int array3[] = {1, 2, 2, 3, 4, 5};  //wrongly use >= and >
   size_t n4 = 5;
   int array4[] = {1, 1, 1, 1, 1};
   size_t n5 = 7;
@@ -25,12 +25,12 @@ int main(void) {
     return EXIT_FAILURE;
   }
 
-  ans = 4;
+  ans = 5;
   if (maxSeq(array2, n2) != ans) {
     printf("test case2,expect %lu, get %lu \n", ans, maxSeq(array2, n2));
     return EXIT_FAILURE;
   }
-
+  ans = 4;
   if (maxSeq(array3, n3) != ans) {
     printf("test case3,expect %lu, get %lu \n", ans, maxSeq(array3, n3));
     return EXIT_FAILURE;
