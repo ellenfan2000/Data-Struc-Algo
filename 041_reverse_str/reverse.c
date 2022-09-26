bb24ca8c9f880b329f86cd990b0cfb16038f6876
rf96 <rf96@duke.edu>
2022-09-25 22:16:23 -0400

    answer 3

--- 041_reverse_str/reverse.c
+++ 041_reverse_str/reverse.c
@@ -4,15 +4,17 @@
 
 void reverse(char * str) {
   //WRITE ME!
-  char curr;
-  int size = 0;
-  while (str[size] != '\0') {
-    size++;
-  }
-  for (int i = 0; i < size / 2; i++) {
-    curr = str[i];
-    str[i] = str[size - 1 - i];
-    str[size - 1 - i] = curr;
+  if (str != NULL) {
+    char curr;
+    int size = 0;
+    while (str[size] != '\0') {
+      size++;
+    }
+    for (int i = 0; i < size / 2; i++) {
+      curr = str[i];
+      str[i] = str[size - 1 - i];
+      str[size - 1 - i] = curr;
+    }
   }
 }
 
@@ -25,10 +27,9 @@ int main(void) {
   char str5[] = "You can be my wingman anyday!";
   char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be "
                 "no tomorrow!";
-  char str7[] = "1 2 3 4 4 5"
-                " 6 7 8 8";
-  char * array[] = {str0, str1, str2, str3, str4, str5, str6, str7};
-  for (int i = 0; i < 8; i++) {
+  //char str7[] = "1 2 3 4 4 5"" 6 7 8 8";
+  char * array[] = {str0, str1, str2, str3, str4, str5, str6};
+  for (int i = 0; i < 7; i++) {
     reverse(array[i]);
     printf("%s\n", array[i]);
   }
