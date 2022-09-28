9f56d4ef8b829e0480e1c6d482cfbbee420f5223
rf96 <rf96@duke.edu>
2022-09-28 17:52:12 -0400

    049

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define LINESIZE 12
+#define LINESIZE 10
+
 int main(int argc, char ** argv) {
   if (argc != 2) {
     fprintf(stderr, "Usage: rotateMatrix inputFileName\n");
@@ -9,7 +10,7 @@ int main(int argc, char ** argv) {
   }
   FILE * f = fopen(argv[1], "r");
   if (f == NULL) {
-    perror("Could not open file");
+    perror("Could not open file\n");
     return EXIT_FAILURE;
   }
   char matrix[LINESIZE][LINESIZE];
@@ -18,20 +19,20 @@ int main(int argc, char ** argv) {
 
   while (fgets(line, LINESIZE + 2, f) != NULL) {
     if (inputSize > 9) {
-      printf("Too mush rows!");
+      printf("Too mush rows!\n");
       return EXIT_FAILURE;
     }
     if (line[10] != '\n') {
-      printf("line is either too long or too short!");
+      printf("line is either too long or too short!\n");
       return EXIT_FAILURE;
     }
-    for (int j = 0; j < 10; j++) {
+    for (int j = 0; j < LINESIZE; j++) {
       matrix[j][10 - inputSize - 1] = line[j];
     }
     inputSize++;
   }
-  if (inputSize != 9) {
-    printf("Too Less lines");
+  if (inputSize < 9) {
+    printf("Too Less lines\n");
     return EXIT_FAILURE;
   }
   for (int i = 0; i < 10; i++) {
