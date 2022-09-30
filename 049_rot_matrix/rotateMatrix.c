de184e24e49d43b468924c10487f04c42736cb75
rf96 <rf96@duke.edu>
2022-09-30 00:27:49 -0400

    use exit

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -3,6 +3,41 @@
 #include <string.h>
 #define LINESIZE 10
 
+void rotate(FILE * f, char matrix[][LINESIZE]) {
+  int inputSize = 0;
+  char line[LINESIZE + 2];
+  while (fgets(line, LINESIZE + 2, f) != NULL) {
+    if (inputSize > 9) {
+      // fclose(f);
+      perror("Too mush rows!\n");
+      exit(EXIT_FAILURE);
+    }
+    if (strchr(line, '\n') == NULL) {
+      // fclose(f);
+      perror("line is too long! \n");
+      exit(EXIT_FAILURE);
+    }
+    if (line[10] != '\n') {
+      //fclose(f);
+      perror("line is too short!\n");
+      exit(EXIT_FAILURE);
+    }
+    for (int j = 0; j < LINESIZE; j++) {
+      matrix[j][10 - inputSize - 1] = line[j];
+    }
+    inputSize++;
+  }
+  if (inputSize < 9) {
+    //fclose(f);
+    perror("Too Less lines\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (fclose(f) != 0) {
+    perror("failed to close file\n");
+    exit(EXIT_FAILURE);
+  }
+}
 int main(int argc, char ** argv) {
   if (argc != 2) {
     fprintf(stderr, "Usage: rotateMatrix inputFileName\n");
@@ -15,40 +50,41 @@ int main(int argc, char ** argv) {
   }
 
   char matrix[LINESIZE][LINESIZE];
-  char line[LINESIZE + 2];
-  int inputSize = 0;
+  rotate(f, matrix);
+  /* char line[LINESIZE + 2]; */
+  /* int inputSize = 0; */
 
-  while (fgets(line, LINESIZE + 2, f) != NULL) {
-    if (inputSize > 9) {
-      // fclose(f);
-      perror("Too mush rows!\n");
-      return EXIT_FAILURE;
-    }
-    if (strchr(line, '\n') == NULL) {
-      // fclose(f);
-      perror("line is too long! \n");
-      return EXIT_FAILURE;
-    }
-    if (line[10] != '\n') {
-      //fclose(f);
-      perror("line is too short!\n");
-      return EXIT_FAILURE;
-    }
-    for (int j = 0; j < LINESIZE; j++) {
-      matrix[j][10 - inputSize - 1] = line[j];
-    }
-    inputSize++;
-  }
-  if (inputSize < 9) {
-    //fclose(f);
-    perror("Too Less lines\n");
-    return EXIT_FAILURE;
-  }
+  /* while (fgets(line, LINESIZE + 2, f) != NULL) { */
+  /*   if (inputSize > 9) { */
+  /*     // fclose(f); */
+  /*     perror("Too mush rows!\n"); */
+  /*     return EXIT_FAILURE; */
+  /*   } */
+  /*   if (strchr(line, '\n') == NULL) { */
+  /*     // fclose(f); */
+  /*     perror("line is too long! \n"); */
+  /*     return EXIT_FAILURE; */
+  /*   } */
+  /*   if (line[10] != '\n') { */
+  /*     //fclose(f); */
+  /*     perror("line is too short!\n"); */
+  /*     return EXIT_FAILURE; */
+  /*   } */
+  /*   for (int j = 0; j < LINESIZE; j++) { */
+  /*     matrix[j][10 - inputSize - 1] = line[j]; */
+  /*   } */
+  /*   inputSize++; */
+  /* } */
+  /* if (inputSize < 9) { */
+  /*   //fclose(f); */
+  /*   perror("Too Less lines\n"); */
+  /*   return EXIT_FAILURE; */
+  /* } */
 
-  if (fclose(f) != 0) {
-    perror("failed to close file\n");
-    return EXIT_FAILURE;
-  }
+  /* if (fclose(f) != 0) { */
+  /*   perror("failed to close file\n"); */
+  /*   return EXIT_FAILURE; */
+  /* } */
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
       printf("%c", matrix[i][j]);
