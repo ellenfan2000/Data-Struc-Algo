1432a34e151e03fa598d284432e2862e6b0845ba
rf96 <rf96@duke.edu>
2022-10-25 21:40:18 -0400

    060 test

--- 060_eval2/rand_story.c
+++ 060_eval2/rand_story.c
@@ -35,7 +35,7 @@ char * fillaBlank(char * line, catarray_t * arr, category_t * pw, int arg) {
   char * word;
   char * endstr = NULL;
   long num = strtol(category, &endstr, 10);
-  if (endstr[0] != '\0') {
+  if (endstr[0] != '\0' || blank_len == 1) {
     const char * word_con = chooseWord(category, arr);
     word = strdup(word_con);
     if (arg == 1) {
