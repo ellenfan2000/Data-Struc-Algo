c68044e98e5cb5058d57af6c1efd0727d58fa638
rf96 <rf96@duke.edu>
2022-10-26 13:48:47 -0400

    060 test

--- 060_eval2/rand_story.c
+++ 060_eval2/rand_story.c
@@ -36,7 +36,7 @@ char * fillaBlank(char * line, catarray_t * arr, category_t * pw, int arg) {
   long num = strtol(category, &endstr, 10);
 
   //if category is not a pure numebr or number is 0
-  if (endstr[0] != '\0' || blank_len == 1 || num == 0) {
+  if (endstr[0] != '\0' || blank_len == 1 || num < 1) {
     const char * word_con = chooseWord(category, arr);
     word = strdup(word_con);
     //remove words if -n
