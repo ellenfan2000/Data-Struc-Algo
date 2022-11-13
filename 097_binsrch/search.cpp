651ee6f06bd95c27f5bf1cec6a49b1544f88c67e
rf96 <rf96@duke.edu>
2022-11-13 00:22:13 -0500

    097

--- 097_binsrch/search.cpp
+++ 097_binsrch/search.cpp
@@ -5,19 +5,16 @@
 #include "function.h"
 
 int binarySearchForZero(Function<int, int> * f, int low, int high) {
-  if (low == high || f->invoke(low) > 0) {
+  if (low == high || low == high - 1) {
     return low;
-  }
-  else if (f->invoke(high) < 0) {
-    return high - 1;
   }
   else {
     int mid = (high + low) / 2;
-
-    if (f->invoke(mid) > 0) {
+    int val = f->invoke(mid);
+    if (val > 0) {
       return binarySearchForZero(f, low, mid);
     }
-    else if (f->invoke(mid) == 0) {
+    else if (val == 0) {
       return mid;
     }
     else {
