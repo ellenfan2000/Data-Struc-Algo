16a9fdbd3b9bb8a83bcc09b29554dd4562021eb4
rf96 <rf96@duke.edu>
2022-11-22 01:45:24 -0500

    103 answer

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,19 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq;
+  for (int i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  while (pq.size() > 1) {
+    Node * first = pq.top();
+    pq.pop();
+    Node * second = pq.top();
+    pq.pop();
+    pq.push(new Node(first, second));
+  }
+  return pq.top();
+}
