8676b2360baade9abb92970477ef5561618e99c2
rf96 <rf96@duke.edu>
2022-11-22 02:01:18 -0500

    104 answer

--- 104_huff_map/node.cpp
+++ 104_huff_map/node.cpp
@@ -1,5 +1,18 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
   //WRITE ME!
+  if (sym != NO_SYM) {
+    assert(left == NULL && right == NULL);
+    theMap.insert(std::pair<unsigned, BitString>(sym, b));
+  }
+  else {
+    if (left != NULL) {
+      BitString bl = b.plusZero();
+      left->buildMap(bl, theMap);
+    }
+    if (right != NULL) {
+      BitString br = b.plusOne();
+      right->buildMap(br, theMap);
+    }
+  }
 }
-
