6b88e85891e91ab3c8ca034de232be047a5f69b8
rf96 <rf96@duke.edu>
2022-12-08 04:37:00 -0500

    step4

--- 093_eval3/cyoa-step3.cpp
+++ 093_eval3/cyoa-step3.cpp
@@ -6,12 +6,28 @@
 int main(int argc, char ** argv) {
   if (argc == 2) {
     std::string dir = argv[1];
-    Story sto(dir);
-
-    if (sto.checkValidStory()) {
-      // sto.printStory(dir);
-      sto.findWin();
+    try {
+      Story sto(dir);
+      if (sto.checkValidStory()) {
+        // sto.printStory(dir);
+        sto.findWin();
+      }
+      else {
+        std::cerr << "Not playable story" << std::endl;
+      }
     }
+    catch (InvalidStoryFile & e) {
+      std::cerr << e.what() << std::endl;
+      return EXIT_FAILURE;
+    }
+    catch (CannotOpenFile & e) {
+      std::cerr << e.what() << std::endl;
+      return EXIT_FAILURE;
+    }
+  }
+  else {
+    std::cerr << "Usage: program dir" << std::endl;
+    return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
 }
